c03f8fce4fca523f6dc85beffde9b3cfa2125984
Rium <tuomassani@hotmail.com>
2026-09-19 16:11:04 +0300

    Added login functionality

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import sqlite3
 from flask import Flask
-from flask import redirect, render_template, request
-from werkzeug.security import generate_password_hash
+from flask import redirect, render_template, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
 import db
+import config
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
@@ -30,3 +32,26 @@ def create():
         return render_template("used.html")
 
     return render_template("done.html")
+
+@app.route("/login_page")
+def login_page():
+    return render_template("login_page.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    sql = "SELECT password_hash FROM users WHERE username = ?"
+    password_hash = db.query(sql, [username])[0][0]
+
+    if check_password_hash(password_hash, password):
+        session["username"] = username
+        return redirect("/")
+    else:
+        return render_template("login_error.html")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
